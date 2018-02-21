e65853d68e2f4f9273a9ff86cc1e402f5a49474c
TimBiden <timbiden@gmail.com>
2018-02-21 12:35:49 -0800

    Begin #10.

---
+++ books.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE books (
+    isbn integer,
+    title varchar,
+    author varchar
+);
+
+INSERT INTO books
+    VALUES
+    (0786222727, "Harry Potter and the Sorcerer's Stone", 'J. K. Rowling'),
+    (0896436809, "Benny's Home", 'Giovanna Dominguez'),
+    (0618645616, "The Lord of the Rings", 'J.R.R. Tolkien'),
+    (0395489334, "The Two Towers", 'J.R.R. Tolkien'),
+    (1941142990, "The 1-Page Marketing Plan", 'Allan Dib'),
+    (1119362415, "The New Rules of Marketing and PR", 'David Meerman Scott'),
+    (1520827105, "Rubik's Cube Best Algorithms: Top 5 Speedcubing Methods", 'Daniel Ross'),
+    (1484757386, "Art of Coloring Star Wars", 'Catherine Saunier-Talec'),
+    (0240819152, "Audio Engineering 101: A Beginner's Guide to Music Production", 'Tim Dittmar'),
+    (0240520149, "Loudspeakers: For music recording and reproduction", 'Philip Richard Newell'),
+    (0321992474, "SQL Queries for Mere Mortals", 'John L. Viescas');
